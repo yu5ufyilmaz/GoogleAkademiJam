37daabc2aa9a142490e77cf67841f377ec6c3dde
Yusuf Yilmaz <ysfyilmaz6@gmail.com>
2023-04-10 01:25:49 +0300

    Level22

--- Gamejam/Assets/DamageAsit.cs
+++ Gamejam/Assets/DamageAsit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBulletScript2 : MonoBehaviour
+public class DamageAsit : MonoBehaviour
 {
     public float speed;
 
@@ -10,8 +10,6 @@ public class EnemyBulletScript2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.velocity = transform.right * -speed;
         Destroy(gameObject,4f);
     }
     
